6a2a6032324ef4e8c1fadd6897e27fcc35116c4f
John Kang <johnkang1121@gmail.com>
2022-12-07 14:04:37 -0800

    added code to get inquirer to ask prompts, trying to figure out how to get html file created

--- index.js
+++ index.js
@@ -4,49 +4,119 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
-const employee = [];
-const position = employee.role
+const employees = [];
+// const position = employees.role
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
-inquirer.prompt([
-   {
-    type: 'list',
+// function newEmployee(){
+    inquirer.prompt([
+    {
+       type: 'list',
     message:'What position is this employee?',
     name: 'position',
     choices: [
         'Manager',
         'Intern',
         'Engineer',
-    ]
-   },
+        ]
+    },
 
-   {
-    type: 'input',
-    message: 'what is the name of the employee?',
-    name: 'name',
-   },
+    {
+        type: 'input',
+        message: 'what is the name of the employee?',
+        name: 'name',
+    },
 
-   {
-    type: 'input',
-    message: 'what is the id of the employee?',
-    name: 'id',
-   },
-
-   {
-    type: 'input',
-    message: 'what is the email of the employee?',
-    name: 'email',
-   },
+    {
+        type: 'input',
+        message: 'what is the id of the employee?',
+        name: 'id',
+    },
    
-]).then(({ position })) => {
-  switch (position) {
-    case 'Manager':
-        // what is officeNumber
-    case 'Intern':
-        // what school 
-    case 'Engineer':
-        // what is their github 
-    default :
-        // uh oh
-  }  
-}
+    {
+       type: 'input',
+       message: 'what is the email of the employee?',
+       name: 'email',
+    },
+    
+]).then(({ position, name, id, email }) => {
+    switch (position) {
+        case 'Manager':
+            // what is officeNumber
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is the office number?',
+                    name: 'officeNumber',
+                }
+            ]).then(({ officeNumber }) => {
+                employees.push (new Manager(
+                    name,
+                    id,
+                    email,
+                    officeNumber,
+                    ))
+                    otherEmployee()
+                })
+                break;
+                
+                case 'Intern':
+                    // what school 
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What school doess the intern attend?',
+            name: 'school',
+        }
+    ]).then(({ school }) => {
+        employees.push (new Intern(
+            name,
+            id,
+            email,
+            school,
+            ))
+            otherEmployee()
+        });
+        break;
+        
+        case 'Engineer':
+            // what is their github 
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: "What is the Engineer's github user ID?",
+                    name: 'github',
+                }
+            ]).then(({ github }) => {
+                employees.push (new Engineer(
+                    name,
+                    id,
+                    email,
+                    github,
+                    ))
+                    otherEmployee()
+                });
+                break;
+                default :
+                // uh oh
+            }  
+        })
+    
+        
+        function otherEmployee(){
+            return inquirer.prompt([
+                {
+                    type: 'confirm',
+                    message: 'Do you want to add more employees?',
+                    name: 'otherEmployee',
+                }
+            ]).then(({ otherEmployee }) => {
+                if (otherEmployee) newEmployee()
+                else renderHTMLFile();
+            })
+        }
+        function renderHTMLFile() {
+            fs.writeFileSync('./index.html')
+        }
+
+        
