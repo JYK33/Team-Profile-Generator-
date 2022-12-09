ff1b9948bc22fcfca16cab49deb0435e9ffbfee7
John Kang <johnkang1121@gmail.com>
2022-12-08 17:57:03 -0800

    added code to index.js, got js to create html

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const employees = [];
 // const position = employees.role
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
-// function newEmployee(){
+function newEmployee(){
     inquirer.prompt([
     {
        type: 'list',
@@ -102,21 +102,71 @@ const employees = [];
             }  
         })
     
-        
-        function otherEmployee(){
-            return inquirer.prompt([
-                {
-                    type: 'confirm',
-                    message: 'Do you want to add more employees?',
-                    name: 'otherEmployee',
-                }
-            ]).then(({ otherEmployee }) => {
-                if (otherEmployee) newEmployee()
-                else renderHTMLFile();
-            })
+}        
+function otherEmployee(){
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            message: 'Do you want to add more employees?',
+            name: 'otherEmployee',
         }
-        function renderHTMLFile() {
-            fs.writeFileSync('./index.html')
+        ]).then(({ otherEmployee }) => {
+            if (otherEmployee) newEmployee()
+            else renderHTMLFile();
+        })
         }
+function renderHTMLFile() {
+    fs.writeFileSync('./index.html', /*html*/`
 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title> Our Team </title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    
+    <div class="jumbotron" style="text-align: center; border-bottom: 5px solid black; background : skyblue; color:black; text-align:center; " id="jumbotron">
+     <h1 class= "display-4" style="font-weight:bolder">Meet The Team!</h1>
+     <h1><i class="fa fa-users" style="font-size:60px"></i></h1>
+    </div>
+    <div class="container">
+    <div class="row">
+
+        
+    ${employees.map(employees => `
+    <div class="col-md-3 text-dark" style = "margin : 5px; padding: 0; text-align: center; background-color: wheat; border-radius: 3px; border-color: black; border-style: solid;">
+     <header style = "background : lightblue">
+      <h1>${employees.getRole()}</h1>
+      <h2>
+     </header>
+      <p> Name: ${employees.getName()}</p><br>
+      <p> ID Number: ${employees.getId()}</p><br>
+      <p> E-Mail: ${employees.getEmail()}</p><br>
+      ${roleInfo(employees)}
+    </div>
+    `)}
+    </div>
+    </div>      
+`)
+}
+function roleInfo(employees) {
+    switch (employees.getRole()) {
+        // getting the 
+        case "Manager":
+        return `<p> Office Number: ${employees.getOfficeNumber()}</p>`
+        break;
+        case "Intern":
+        return `<p> School: ${employees.getSchool()}</p>`
+        break;
+        case "Engineer":
+        return `<p> GitHub: <a href ="https://www.github.com/${employees.getGithub()}"> GitHub</a></p>`
+        break;
+    }
+}
+
+newEmployee();
         
